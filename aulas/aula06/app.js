f2a2ad288f330d4334d1928234146487a132220a
mateus rosetti <mateus.rosetti@iesb.edu.br>
2025-09-18 21:48:06 -0300

    final aula 06

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -3,9 +3,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const tarefas = []
-
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
+const tarefaRouter=require("./routes/tarefas");
 
 const app = express();
 
@@ -14,22 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use("/",indexRouter);
+app.use("/tarefas",tarefaRouter);
 
-app.get('/tarefas',(req,res)=>{
-    res.json(tarefas);
-
-app.post('/tarefas',(req,res)=>{
-    const novaTarefa ={...req.body,id:tarefas.length +1};
-    tarefas.push(novaTarefa);
-    res.status(201).json(novaTarefa);
-    });
-app.get("/tarefas/:id",(req,res)=>{
-    const { id } = req.params;
-    const tarefaEncontrada = tarefas.find(item => item.id ===parseInt(id));
-    if(tarefaEncontrada)return res.json(tarefaEncontrada);
-    res.status(404).json({msg:"tarefa nao encontrada"});
-    res.json(tarefaEncontrada);
-  });
-});
 module.exports = app;
--- aulas/aula06/tests/app.test.js
+++ aulas/aula06/tests/app.test.js
@@ -40,4 +40,27 @@ test("GET /id retorna 404",async()=>{
     expect(response.headers["content-type"]).toMatch(/json/)
     expect(response.body['msg']).toBe("tarefa nao encontrada");
  });
+test("PUT/id retorna 200", async ()=>{
+    const response =await request
+        .put(`${url}/${id}`)
+    .send({nome:"estudar para p1", concluida:
+        true});
+    expect(response.status).toBe(200);
+    expect(response.headers["content-type"]).toMatch(/json/)
+    expect(response.body.id).toBe(id);
+    expect(response.body.nome).toBe("estudar para p1");
+    expect(response.body.concluida).toBe(true);
+
+});
+test("DELETE/id retorna 204",async()=>{
+    const response= await request.delete(`${url}/${id}`);
+    expect(response.status).toBe(204);
+    expect(response.body).toStrictEqual({});
+});
+    test("DELETE /id retorna 404",async()=> {
+        const response = await request.delete(`${url}/0`);
+        expect(response.status).toBe(404);
+        expect(response.headers["content-type"]).toMatch(/json/);
+        expect(response.body['msg']).toBe("tarefa nao encontrada");
+  });
 });
