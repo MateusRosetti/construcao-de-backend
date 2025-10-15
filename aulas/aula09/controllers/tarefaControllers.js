24ab8a1a4685a682930809747c7c20fe8be70d5e
mateus rosetti <mateus.rosetti@iesb.edu.br>
2025-10-14 21:58:39 -0300

    aula 09 completa

---
+++ aulas/aula09/controllers/tarefaControllers.js
@@ -0,0 +1,24 @@
+function listar(req,res){
+    return res.json({});
+
+}
+
+function criar(req,res){
+    return res.status(201).json({});
+}
+
+function buscar(req,res){
+    const { id } = req.params;
+    next();
+}
+function exibir(req,res){
+    return res.json({});
+}
+function atualizar(req,res){
+    return res.json({})
+}
+function remover(req,res){
+    return res.status(204).end();
+}
+
+module.exports={listar,criar,buscar,exibir,atualizar,remover};
